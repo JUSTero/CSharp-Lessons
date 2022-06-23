b8084530d8db8e694b5630a6c6fcd175b4db2b11
JUSTero <skorneta@mail.ru>
2022-06-23 22:34:36 +0300

    Task complete

---
+++ CSharp_Seminar_1_T1/CSharp_Seminar_1_T1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Seminar_1_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("И еще одно ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("В этом случае " + a + " больше");
+            }
+
+            else
+            {
+                Console.WriteLine("В этом случае " + b + " больше");
+            }
+        }
+    }
+}
