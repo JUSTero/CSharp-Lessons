e16db6b2bb5c82fa63d74912daa7589de765cbff
JUSTero <skorneta@mail.ru>
2022-06-23 23:08:36 +0300

    Task 4 complete

---
+++ CSharp_Seminar_1_T4/CSharp_Seminar_1_T4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;                                      //еврейское решение
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Seminar_1_T4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваши четные числа");
+
+            for (int i = 2; i <= number; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+/*using System;                                         //законное решение
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Seminar_1_T4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваши четные числа");
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}*/
