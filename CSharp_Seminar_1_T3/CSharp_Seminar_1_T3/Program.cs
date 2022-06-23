0e996e4e54a87be39f1a658b59ca99ad73e3eb29
JUSTero <skorneta@mail.ru>
2022-06-23 22:56:38 +0300

    Task 3 complete

---
+++ CSharp_Seminar_1_T3/CSharp_Seminar_1_T3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Seminar_1_T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайте ваше число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число " + number + " четное");
+            }
+
+            else
+            {
+                Console.WriteLine("Число " + number + " нечетное");
+            }
+        }
+    }
+}
