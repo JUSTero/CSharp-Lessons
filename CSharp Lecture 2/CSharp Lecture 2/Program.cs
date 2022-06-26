ec236684faa5d135ab243908a3799dc528749697
JUSTero <skorneta@mail.ru>
2022-06-26 18:06:43 +0300

    fill and print array

---
+++ CSharp Lecture 2/CSharp Lecture 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Lecture_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)                                   //Fillarray(array);
+                                                                          //Printarray(array);
+        {
+            void Fillarray(int[] length)
+
+            {
+                Random c = new Random();
+
+                for (int i1 = 0; i1 < length.Length; i1++)
+                {
+                    length[i1] = c.Next();
+                }
+            }
+
+            void Printarray(int[] list)
+
+            {
+                for (int i2 = 0; i2 < list.Length; i2++)
+                {
+                    Console.WriteLine(list[i2]);
+                }
+            }
+            int[] array = new int[0];
+            int size = 1;
+            int index = 0;
+            while (true)
+            {
+                string word = Console.ReadLine();                
+
+                if (word != "end")
+                {
+                    int num = Convert.ToInt32(word);
+                    Array.Resize(ref array, size);
+                    array[index] = num;
+                    index++;
+                    size++;
+                }
+                else
+                {
+                    Printarray(array);
+                    break;
+                }
+            }            
+        }
+    }
+}
