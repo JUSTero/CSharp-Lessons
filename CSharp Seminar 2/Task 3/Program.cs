3f4bab62b8ae52be5f2d503ebde363d04e73aa0e
JUSTero <skorneta@mail.ru>
2022-06-28 00:17:53 +0300

    task 3 complete

--- CSharp Seminar 2/Task 3/Program.cs
+++ CSharp Seminar 2/Task 3/Program.cs
@@ -10,6 +10,24 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 7 || num < 1)
+            {
+                Console.WriteLine("Неприемлимое число, введите новое число");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (num == 6 || num == 7)
+            {
+                Console.WriteLine("Выходной!!!");
+            }
+
+            else
+            {
+                Console.WriteLine("Опять работа");
+            }
         }
     }
 }
