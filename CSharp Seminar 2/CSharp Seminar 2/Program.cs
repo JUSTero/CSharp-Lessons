017858ba5ddc62b46bc51e37a05a28fe93235188
JUSTero <skorneta@mail.ru>
2022-06-27 22:16:35 +0300

    create homework project

--- CSharp Seminar 2/CSharp Seminar 2/Program.cs
+++ CSharp Seminar 2/CSharp Seminar 2/Program.cs
@@ -10,17 +10,39 @@ namespace CSharp_Seminar_2
     {
         static void Main(string[] args)
         {
-            int num = new Random().Next(10, 100);
-
-            if (num / 10 > num % 10)
+            /*void numberRandom(int min, int max)
             {
-                Console.WriteLine(num + " ==> " + num / 10);
+                int num = new Random().Next(min, max + 1);
+
+                if (num / 10 > num % 10)
+                {
+                    Console.WriteLine(num + " ==> " + num / 10);
+                }
+
+                else
+                {
+                    Console.WriteLine(num + " ==> " + num % 10);
+                }
             }
 
-            else
+            Console.WriteLine("Введите нижний предел");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхний предел");
+            int end = Convert.ToInt32(Console.ReadLine());
+            numberRandom(start, end);*/
+
+            int sumTwoNumbers(int a, int b)
             {
-                Console.WriteLine(num + " ==> " + num % 10);
+                int sum = a + b;
+                return sum;
             }
+            Console.WriteLine("Введите первое число");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Сумма = " + sumTwoNumbers(number1, number2));
         }
+
     }
 }
