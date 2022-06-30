d40bb536d933869ac528553ea4a5668dc0997fd0
JUSTero <skorneta@mail.ru>
2022-06-30 21:43:29 +0300

    task 3 complete

--- SCharp seminar 3/Task 3/Program.cs
+++ SCharp seminar 3/Task 3/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task_3
+namespace CSharp_Lesson_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i + " => " + i * i);
+
+            }
         }
     }
 }
