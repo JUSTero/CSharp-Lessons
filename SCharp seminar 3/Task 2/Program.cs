509628bc57786bd1870baea389a19aeda66fc2f6
JUSTero <skorneta@mail.ru>
2022-06-30 21:13:47 +0300

    task 2 complete

---
+++ SCharp seminar 3/Task 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int Input()
+            {
+                Console.WriteLine("Введите номер четверти");
+                a = Convert.ToInt32(Console.ReadLine());
+                return a;
+            }
+            Input();
+            while (a > 4 || a < 1)
+            {
+                Console.WriteLine("Введено недопустимое значение");
+                Input();
+            }
+
+            if (a == 1)
+            {
+                Console.WriteLine("x > 0");
+                Console.WriteLine("y > 0");
+            }
+
+            else if (a == 2)
+            {
+                Console.WriteLine("x < 0");
+                Console.WriteLine("y > 0");
+            }
+
+            else if (a == 3)
+            {
+                Console.WriteLine("x < 0");
+                Console.WriteLine("y < 0");
+            }
+
+            else
+            {
+                Console.WriteLine("x > 0");
+                Console.WriteLine("y < 0");
+            }
+        }
+    }
+}
