3518681efa8797180768d0af13d1b37ef6091373
JUSTero <skorneta@mail.ru>
2022-06-30 23:02:13 +0300

    task 4 complete

--- SCharp seminar 3/Task 4/Program.cs
+++ SCharp seminar 3/Task 4/Program.cs
@@ -10,6 +10,16 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите координату Ах");
+            int ax = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Введите координату Аy");
+            int ay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координату Bх");
+            int bx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координату By");
+            int by = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Sqrt(Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2)));
         }
     }
 }
