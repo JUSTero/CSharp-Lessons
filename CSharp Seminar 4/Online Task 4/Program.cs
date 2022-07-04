24ba0ace8cfe441859954facb654f6f3f7f30b7c
JUSTero <skorneta@mail.ru>
2022-07-04 22:06:39 +0300

    task 4 complete

--- CSharp Seminar 4/Online Task 4/Program.cs
+++ CSharp Seminar 4/Online Task 4/Program.cs
@@ -10,6 +10,36 @@ namespace Online_Task_4
     {
         static void Main(string[] args)
         {
+            int[] RandomArrayGenerate(int N, int start, int end)
+            {
+                Random random = new Random();
+                int[] RandomArray = new int[N];
+                for (int i = 0; i < N; i++)
+                {
+                    RandomArray[i] = random.Next(start, end + 1);
+                }
+                return RandomArray;
+            }
+            void ShowArray(int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Введите количество элементов массива");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число случайного генерируемого диапазона");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите последнее число случайного генерируемого диапазона");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            int[] myRandomArray = RandomArrayGenerate(num, min, max);
+            ShowArray(myRandomArray);
         }
+
     }
 }
