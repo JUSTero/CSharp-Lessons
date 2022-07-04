4c059d5083dc83e2245da5abd97fbc4fb0aef1a1
JUSTero <skorneta@mail.ru>
2022-07-05 00:04:08 +0300

    create task project and complete task 1

---
+++ CSharp seminar 4 Homework/Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            int degree = Convert.ToInt32(Console.ReadLine());
+            int temp = num;
+
+            for (int i = 1; i < degree; i++)
+            {
+                temp *= num;
+            }
+
+            Console.WriteLine(temp);
+        }
+    }
+}
