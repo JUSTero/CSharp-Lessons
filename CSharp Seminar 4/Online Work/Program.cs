0871976f56acaf7bec12584b861ae867e282e542
JUSTero <skorneta@mail.ru>
2022-07-04 20:54:05 +0300

    task 1 complete

---
+++ CSharp Seminar 4/Online Work/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            void sum(int num)
+            {
+                int tempSum = 0;
+                for (int i = 1; i <= num; i++)
+                {
+                    tempSum += i;
+                }
+
+                Console.WriteLine(tempSum);
+            }
+
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            sum(number);
+
+        }
+    }
+}
