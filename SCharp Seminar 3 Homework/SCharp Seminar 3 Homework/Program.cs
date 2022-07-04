87ff8851197405df11058597e446579e06428d71
JUSTero <skorneta@mail.ru>
2022-07-04 22:58:18 +0300

    task 1 complete

--- SCharp Seminar 3 Homework/SCharp Seminar 3 Homework/Program.cs
+++ SCharp Seminar 3 Homework/SCharp Seminar 3 Homework/Program.cs
@@ -10,8 +10,27 @@ namespace SCharp_Seminar_3_Homework
     {
         static void Main(string[] args)
         {
-            int[] palindrom = new int[5];
             Console.WriteLine("Введите пятизначное число");
+            string num = (Console.ReadLine());
+            int size = num.Length;
+            int count = 0;
+            for (int i = 0; i <= size / 2; i++)
+            {
+                if (num[i] == num[size - 1])
+                {
+                    size--;
+                    count++;
+                }
+
+                else
+                {
+                    Console.WriteLine("Число не является палиндромом");
+                    break;
+                }
+
+                if (count < size / 2)
+                Console.WriteLine("Число является палиндромом");
+            }
         }
     }
 }
