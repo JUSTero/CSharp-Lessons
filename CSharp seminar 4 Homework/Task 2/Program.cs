b12b4a49afdaf3589adab521daf0e283f86b7321
JUSTero <skorneta@mail.ru>
2022-07-05 00:07:35 +0300

    task 2 complete

--- CSharp seminar 4 Homework/Task 2/Program.cs
+++ CSharp seminar 4 Homework/Task 2/Program.cs
@@ -10,6 +10,17 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
