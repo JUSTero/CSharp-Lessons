cc3b24bb7b567a009e490965503bdeb914b278be
JUSTero <skorneta@mail.ru>
2022-07-04 23:45:28 +0300

    task 3 complete

--- SCharp Seminar 3 Homework/Task 3/Program.cs
+++ SCharp Seminar 3 Homework/Task 3/Program.cs
@@ -10,7 +10,13 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ваше число");
+            int num = Convert.ToInt32(Console.ReadLine());
 
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i + " => " + Math.Pow(i, 3));
+            }
         }
     }
 }
