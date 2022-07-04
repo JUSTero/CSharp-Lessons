5e53a1ea6add706879a641a9287099cb6b874aaf
JUSTero <skorneta@mail.ru>
2022-07-04 23:39:48 +0300

    micro repair task 2

--- SCharp Seminar 3 Homework/Task 2/Program.cs
+++ SCharp Seminar 3 Homework/Task 2/Program.cs
@@ -24,7 +24,7 @@ namespace Task_2
             int z2 = Convert.ToInt32(Console.ReadLine());
 
             double lenght = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-            Console.WriteLine(lenght);
+            Console.WriteLine(Math.Round(lenght, 2));
         }
     }
 }
