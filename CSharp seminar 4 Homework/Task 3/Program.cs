6d47f345bc043e62aa304383e31b44a45cfb9d29
JUSTero <skorneta@mail.ru>
2022-07-05 00:18:53 +0300

    task 3 complete

--- CSharp seminar 4 Homework/Task 3/Program.cs
+++ CSharp seminar 4 Homework/Task 3/Program.cs
@@ -10,6 +10,21 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[8];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1000);
+            }
+
+            Console.Write("Ваш массив => ");
+            
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
