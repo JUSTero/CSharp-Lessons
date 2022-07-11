518320139a8829bbbe9cc22a9670ea1df405e7dd
JUSTero <skorneta@mail.ru>
2022-07-11 23:59:11 +0300

    create and resolve task 1

--- CSharp Lesson 5 Homework/Task 1/Program.cs
+++ CSharp Lesson 5 Homework/Task 1/Program.cs
@@ -10,7 +10,25 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter array lenght");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int[] numbers = new int[lenght];
+            int count = 0;
+            Console.WriteLine("Create array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(100, 1000);
+                Console.Write($"{numbers[i], 5}");
 
+                if (numbers[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Number of even numbers = " + count);
         }
     }
 }
