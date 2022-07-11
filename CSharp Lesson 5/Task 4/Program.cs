11e7ec4c20e8d54e0b5fdbbca24a52fb7e73ac82
JUSTero <skorneta@mail.ru>
2022-07-11 23:09:36 +0300

    create and resolve task 4

---
+++ CSharp Lesson 5/Task 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[123];
+            Console.WriteLine("Create array:");
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1001);
+                Console.Write($"{numbers[i], 5}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >= 10 && numbers[i] <= 99)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"{count, 4} numbers satisfy the condition");
+        }
+    }
+}
