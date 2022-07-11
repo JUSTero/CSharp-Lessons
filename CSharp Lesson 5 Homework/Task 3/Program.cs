b093f05ff15a3e49e9f85f7c0bacfe0f7de6b5c3
JUSTero <skorneta@mail.ru>
2022-07-12 00:20:02 +0300

    create and resolve task 3

--- CSharp Lesson 5 Homework/Task 3/Program.cs
+++ CSharp Lesson 5 Homework/Task 3/Program.cs
@@ -10,6 +10,39 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter array lenght");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter bottom limit");
+            int botLim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter top limit");
+            int topLim = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int[] num = new int[lenght];
+            Console.WriteLine("Create array:");
+            int max = num[0];
+            int min = num[0];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = random.Next(botLim, topLim);
+                Console.Write($"{num[i], 5}");
+            }
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] > max)
+                {
+                    max = num[i];
+                }
+
+                else if (num[i] < min)
+                {
+                    min = num[i];
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result: " + (max - min));
         }
     }
 }
