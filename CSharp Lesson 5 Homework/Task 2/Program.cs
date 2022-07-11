1f958390c894614bc5e3b721ad2051dd671f1172
JUSTero <skorneta@mail.ru>
2022-07-12 00:09:22 +0300

    create and resolve task 2

--- CSharp Lesson 5 Homework/Task 2/Program.cs
+++ CSharp Lesson 5 Homework/Task 2/Program.cs
@@ -10,6 +10,31 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter array lenght");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter bottom limit");
+            int botLim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter top limit");
+            int topLim = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int[] numbers = new int[lenght];
+            int sum = 0;
+            Console.WriteLine("Create array:");
+
+            for (int i = 0; i < lenght; i++)
+            {
+                numbers[i] = random.Next(botLim, topLim);
+                Console.Write($"{numbers[i], 5}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < lenght; i += 2)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("Result: " + sum);
         }
     }
 }
