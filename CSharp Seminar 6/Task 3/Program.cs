12ba824b7c514d63f194c25d35293545bed6e471
JUSTero <skorneta@mail.ru>
2022-07-13 23:00:58 +0300

    task 3 complete

---
+++ CSharp Seminar 6/Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string Binary = string.Empty;
+
+            for (int i = 0; num > 0; i++)
+            {
+                Binary = Convert.ToString(num % 2) + Binary;
+                num /= 2;
+            }
+
+            Console.WriteLine(Binary);
+        }
+    }
+}
