9546c69671ca99acbcfbf4884c298b189994a034
JUSTero <skorneta@mail.ru>
2022-07-13 22:52:17 +0300

    task 2 complete

--- CSharp Seminar 6/Task 2/Program.cs
+++ CSharp Seminar 6/Task 2/Program.cs
@@ -10,6 +10,30 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the numbers");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= y + z)
+            {
+                Console.WriteLine("Triangle impossible");
+            }
+
+            else if (y >= z + x)
+            {
+                Console.WriteLine("Triangle impossible");
+            }
+
+            else if (z >= x + y)
+            {
+                Console.WriteLine("Triangle impossible");
+            }
+
+            else
+            {
+                Console.WriteLine("Triangle possible");
+            }
         }
     }
 }
