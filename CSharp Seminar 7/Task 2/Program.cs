33005daab44fb978d51327c2814848d3f2aa502b
JUSTero <skorneta@mail.ru>
2022-07-17 13:05:33 +0300

    task 2 complete

---
+++ CSharp Seminar 7/Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of lines");
+            int lines = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of columns");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] numbers = new int[lines, columns];
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(0); j++)
+                {
+                    numbers[i, j] = i + j;
+                    Console.Write($"{numbers[i, j],5}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
