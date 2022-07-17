01f798291079f0084dde3488e13a2d4c497c871c
JUSTero <skorneta@mail.ru>
2022-07-17 10:58:19 +0300

    task 4 complete

---
+++ CSharp Seminar 6/Task 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter count of numbers");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[num];
+            Console.WriteLine("Fibonacci sequence:");
+
+            if (num == 2)
+            {
+                Console.Write($"0  1");
+                Console.WriteLine();
+            }
+
+            else if (num == 1)
+            {
+                Console.Write("0");
+                Console.WriteLine();
+            }
+
+            else if (num > 2)
+            {
+                Console.Write($"0  1 ");
+                numbers[1] = 1;
+                for (int i = 2; i < num; i++)
+                {
+                    numbers[i] = numbers[i - 2] + numbers[i - 1];
+                    Console.Write($" {numbers[i]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
