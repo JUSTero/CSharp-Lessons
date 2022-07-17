91a2884e67ae2dd32238d0fedd215cf4e916084f
JUSTero <skorneta@mail.ru>
2022-07-17 12:08:07 +0300

    task 2 complete

--- CSharp seminar 6 Homework/Task 2/Program.cs
+++ CSharp seminar 6 Homework/Task 2/Program.cs
@@ -10,6 +10,28 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter k1");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter k2");
+            double k2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b1");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b2");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+
+            if (k1 == k2)
+            {
+                Console.WriteLine("No intersection point. Lines are parallel");
+            }
+
+            else
+            {
+                double x = (b2 - b1) / (k1 - k2);
+                double y1 = k1 * x + b1;
+                double y2 = k2 * x + b2;
+
+                Console.WriteLine("Intersection point coordinates: x = " + x + " y = " + y1);
+            }
         }
     }
 }
