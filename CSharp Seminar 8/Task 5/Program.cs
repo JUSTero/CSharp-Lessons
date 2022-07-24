8f9c0183447c1aaab17f5c2518c47c7fd44d40d9
JUSTero <skorneta@mail.ru>
2022-07-24 18:32:56 +0300

    task 5 hard

---
+++ CSharp Seminar 8/Task 5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of lines");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int factorial(int m)
+            {
+                int x = 1;
+
+                for (int i = 1; i <= m; i++)
+                {
+                    x *= i;
+                }
+
+                return x;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= (n - i); j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write(" ");
+                    Console.Write(factorial(i) / (factorial(k) * factorial(i - k)));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
