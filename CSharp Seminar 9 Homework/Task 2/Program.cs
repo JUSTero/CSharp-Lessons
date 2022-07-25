747ca00e29a443f06c92517e64347967a0d1ef8f
JUSTero <skorneta@mail.ru>
2022-07-25 23:10:44 +0300

    task 2 complete

---
+++ CSharp Seminar 9 Homework/Task 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower number");
+            int lowerNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the upper number");
+            int upperNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int sum = 0;
+
+            if (upperNumber < lowerNumber)
+            {
+                int temp = lowerNumber;
+                lowerNumber = upperNumber;
+                upperNumber = temp;
+            }
+
+            int sumOfNumbers(int m, int n)
+            {
+                if (m <= n)
+                {
+                    sum += m;
+                    sumOfNumbers(m + 1, n);
+                }
+
+                return sum;
+            }
+
+            sumOfNumbers(lowerNumber, upperNumber);
+            Console.WriteLine($"Sum of numbers = {sum}");
+            Console.WriteLine();
+        }
+    }
+}
