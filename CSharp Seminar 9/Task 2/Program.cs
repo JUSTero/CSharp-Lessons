27b76dd8a00a5fd06e5dd76e94b72a7338df7cb7
JUSTero <skorneta@mail.ru>
2022-07-25 22:10:38 +0300

    task 2 complete

---
+++ CSharp Seminar 9/Task 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower number");
+            int lowerNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the upper number");
+            int upperNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"M = {lowerNumber}; N = {upperNumber} -> ");
+
+            void Subsequence(int m, int n)
+            {
+                if (m < n)
+                {
+                    Console.Write($"{m} ");
+                    Subsequence(m + 1, n);
+                }
+
+                else
+                {
+                    Console.Write(m);
+                }
+            }
+
+            if (upperNumber < lowerNumber)
+            {
+                int temp = lowerNumber;
+                lowerNumber = upperNumber;
+                upperNumber = temp;
+            }
+
+            Subsequence(lowerNumber, upperNumber);
+            Console.WriteLine();
+
+        }
+    }
+}
