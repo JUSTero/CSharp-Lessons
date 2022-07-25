91f385340158f4dff2ed88e3937f8803e0950c18
JUSTero <skorneta@mail.ru>
2022-07-25 23:25:22 +0300

    task 3 complete

---
+++ CSharp Seminar 9 Homework/Task 3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower number");
+            int lowerNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the upper number");
+            int upperNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int Akkerman(int m, int n)
+            {
+                if (m == 0)
+                {
+                    return n + 1;
+                }
+
+                else if (m > 0 && n == 0)
+                {
+                    return Akkerman(m - 1, 1);
+                }
+
+                else if (m > 0 && n > 0)
+                {
+                    return Akkerman(m - 1, Akkerman(m, n -1));
+                }
+
+                else
+                {
+                    return 0;
+                }    
+            }
+
+            int akkermanFunction = Akkerman(lowerNumber, upperNumber);
+            Console.WriteLine($"Result = {akkermanFunction}");
+            Console.WriteLine();
+
+        }
+    }
+}
