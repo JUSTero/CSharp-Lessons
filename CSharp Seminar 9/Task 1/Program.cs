c412ef1540cba77cb9e7ae13052630174d06e866
JUSTero <skorneta@mail.ru>
2022-07-25 22:00:25 +0300

    task 1 complete

---
+++ CSharp Seminar 9/Task 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"N = {N} -> ");
+            int i = 1;
+
+            void Subsequence(int n)
+            {
+                if (i < n)
+                {
+                    Console.Write($"{i} ");
+                    i++;
+                    Subsequence(n);
+                }
+
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+
+            Subsequence(N);
+            Console.WriteLine();
+        }
+    }
+}
