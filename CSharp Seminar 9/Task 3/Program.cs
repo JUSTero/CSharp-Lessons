dcd415aaf835accee45e3ee1a48acaee4e70d49e
JUSTero <skorneta@mail.ru>
2022-07-25 22:35:30 +0300

    task 3 complete

---
+++ CSharp Seminar 9/Task 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            int SumOfNumbers(int n)
+            {
+                if (n / 10 > 0)
+                {
+                    sum += n % 10;
+                    SumOfNumbers(n / 10);
+                }
+
+                else
+                {
+                    sum += n;
+                }
+
+                return sum;
+            }
+
+            SumOfNumbers(number);
+            Console.WriteLine($"Sum of digits = {sum}");
+        }
+    }
+}
+    
