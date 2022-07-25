443adec03126e31f899a0c8fce977b0b0fc41b16
JUSTero <skorneta@mail.ru>
2022-07-25 22:50:46 +0300

    task 4 complete

---
+++ CSharp Seminar 9/Task 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter degree");
+            int degree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int i = 1;
+            int result = 1;
+
+            int Exponentiation(int n, int d)
+            {
+                if (i <= d)
+                {
+                    result *= n;
+                    i++;
+                    Exponentiation(n, d);
+                }
+
+                return result;
+            }
+
+            Console.Write($"{number} To the power of {degree} = ");
+            Exponentiation(number, degree);
+            Console.WriteLine(result);
+            Console.WriteLine();
+        }
+    }
+}
